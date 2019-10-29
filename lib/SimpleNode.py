c6b5fa10a28fb26836f844a20d19bfaf8e4eca44
ctony <ctianyi7@gmail.com>
2019-10-29 11:01:13 -0400

    Modify data structure

--- lib/SimpleNode.py
+++ lib/SimpleNode.py
@@ -1,20 +1,20 @@
 class SimpleNode:
-    Cdegree = None
-    qdegree = None
-    # {'name of neighbor':[C*w+ - w-, w+ - q*w-]}
+    C_degree = None
+    positive_degree = None
+    # {'name of neighbor':[C*w+ - w-, w+]}
     neighbor_dict = None
     fib_node = None
 
     def __init__(self):
-        self.Cdegree = 0
-        self.qdegree = 0
+        self.C_degree = 0
+        self.positive_degree = 0
         self.neighbor_dict = {}
 
-    def increase_neighbor(self, name, Cdegree, qdegree):
+    def increase_neighbor(self, name, Cdegree, positive_degree):
         if name not in self.neighbor_dict:
-            self.neighbor_dict[name] = [Cdegree, qdegree]
+            self.neighbor_dict[name] = [Cdegree, positive_degree]
         else:
             self.neighbor_dict[name][0] += Cdegree
-            self.neighbor_dict[name][1] += qdegree
-        self.qdegree += qdegree
-        self.Cdegree += Cdegree
+            self.neighbor_dict[name][1] += positive_degree
+        self.positive_degree += positive_degree
+        self.C_degree += Cdegree
